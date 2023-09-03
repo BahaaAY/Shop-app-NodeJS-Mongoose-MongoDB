29dcdfd5b5032798902b2b3f2c828d6bea27a967
Bahaa Abou Yassine <bahaa.abouyassine2@gmail.com>
2023-09-03 13:37:37 +0300

    🌍 Using Session and Storing it in MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const username = require("./util/credentials").username;
 const password = require("./util/credentials").password;
@@ -13,14 +15,30 @@ const User = require("./models/user");
 
 const app = express();
 
+const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0.o8mxmhh.mongodb.net/shop`;
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    store: store,
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use((req, res, next) => {
   console.log("Url:", req.url);
@@ -37,13 +55,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    `mongodb+srv://${username}:${password}@cluster0.o8mxmhh.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     return User.findOne();
   })
@@ -54,8 +71,13 @@ mongoose
         email: "Bahaa@email.com",
         cart: { items: [] },
       });
-      user.save();
+      return user.save();
+    } else {
+      return user;
     }
+  })
+  .then((result) => {
+    console.log("Connected!");
     app.listen(3000);
   })
   .catch((err) => {
